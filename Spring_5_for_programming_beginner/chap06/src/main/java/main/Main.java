850ec75ff82c8ea9c6cfbe2984f0c0cc02f2822c
dddalang4 <dddalang4@naver.com>
2022-04-10 15:19:15 +0900

    챕터 6 : '빈 라이프사이클과 범위' 정리

---
+++ Spring_5_for_programming_beginner/chap06/src/main/java/main/Main.java
@@ -0,0 +1,19 @@
+package main;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+
+import config.AppCtx;
+import spring.Client;
+import spring.Client2;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		AbstractApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class);
+		Client2 client = ctx.getBean(Client2.class);
+		client.send();
+		
+		ctx.close();
+	}
+}
